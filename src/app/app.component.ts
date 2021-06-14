b9020fa1488cf040165161dd5e59ffae7f025b98
David <unbekannt.ghost@gmail.com>
2021-06-14 18:45:08 +0200

    remove comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,109 +24,12 @@ export class AppComponent implements OnInit {
 
   constructor(private db: Repository, private http: HttpClient) {
     this.db = db;
-    // TODO fix dg.getAllClasses
-    // TODO define standard schoolclass (select)
-    // TODO define correct teacher
     this.title = 'stundenplan';
-    /*this.listofunitsserver = [
-      {
-        id: 4,
-        day: 1,
-        unit: 3,
-        subject: 'INSY',
-        teacherID: 3,
-        schoolclassID: '5BHITM'
-      },
-      {
-        id: 5,
-        day: 1,
-        unit: 4,
-        subject: 'ITP',
-        teacherID: 3,
-        schoolclassID: '5BHITM'
-      },
-      {
-        id: 6,
-        day: 1,
-        unit: 5,
-        subject: 'ITP',
-        teacherID: 3,
-        schoolclassID: '5BHITM'
-      },
-      {
-        id: 4,
-        day: 2,
-        unit: 3,
-        subject: 'INSY',
-        teacherID: 3,
-        schoolclassID: '5BHITM'
-      },
-      {
-        id: 4,
-        day: 3,
-        unit: 3,
-        subject: 'INSY',
-        teacherID: 3,
-        schoolclassID: '5BHITM'
-      },
-      {
-        id: 4,
-        day: 4,
-        unit: 3,
-        subject: 'INSY',
-        teacherID: 3,
-        schoolclassID: '5BHITM'
-      }, {
-        id: 4,
-        day: 5,
-        unit: 3,
-        subject: 'INSY',
-        teacherID: 3,
-        schoolclassID: '5BHITM'
-      },
-    ];
-    this.listofclasses = [
-      {
-        id: '3BHITM',
-        room: '137'
-      },
-      {
-        id: '4BHITM',
-        room: '136'
-      },
-      {
-        id: '5BHITM',
-        room: '135'
-      }
-    ];
-    this.listofteachers = [
-      {
-        id: 1,
-        firstname: 'Gerald',
-        lastname: 'Aistleitner',
-        room: 'U12'
-      },
-      {
-        id: 2,
-        firstname: 'Herbert',
-        lastname: 'Lackinger',
-        room: '221'
-      },
-      {
-        id: 3,
-        firstname: 'Johannes',
-        lastname: 'Tumfahrt',
-        room: 'E42'
-      }
-    ];*/
   }
 
-
   ngOnInit(): void {
-    console.log('ngOnInit');
     this.db.getAllClasses().subscribe(
       (val) => {
-        console.log(val);
         this.listofclasses = val;
         this.getUnitsbyClassname(this.currentschoolclass);
       }
@@ -136,8 +39,8 @@ export class AppComponent implements OnInit {
   // tslint:disable-next-line:typedef
   checkIfUnitExists(row, column) {
     if (this.listofunitsserver != null) {
-      for (let unit of this.listofunitsserver) {
-        if (unit.day == column && unit.unit == row && unit.schoolclassID == this.currentschoolclass) {
+      for (const unit of this.listofunitsserver) {
+        if (unit.day === column && unit.unit === row && unit.schoolclassID === this.currentschoolclass) {
           this.currentunit = unit;
           return true;
         }
